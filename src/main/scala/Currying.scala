131c97a75983eb32ffd41cef52a533fdad641d4b
ShivaniGanatra <shivaniganatra@live.com>
2025-10-07 13:31:42 +0100

    Finished currying exercise

---
+++ src/main/scala/Currying.scala
@@ -0,0 +1,13 @@
+object Currying extends App{
+
+    def add2 (x:Int) = (y:Int) => x + y
+    println(add2(3)(4)) //7
+
+    val aSequence : Array[Int] = Array(1,2,3,4)
+
+  println(aSequence.map(_ + 1)) // List(2, 3, 4)
+
+    def multiplyAll(x : Array[Int]) = (y:Int) => x.map(x => x * y).mkString("Array(", ", ", ")")
+
+    println(multiplyAll(Array(1, 2, 3,4,3,4,55))(4))
+}
